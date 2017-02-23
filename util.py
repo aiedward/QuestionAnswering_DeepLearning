2182d08f963572c16c002ac78687e26892393ef5
Johanna Chan <8nahchan8@gmail.com>
2017-02-23 09:52:34 -0400

    Actualización (23-02-2017 9:52 AM)

--- util.py
+++ util.py
@@ -1,17 +1,18 @@
 import json
 #from pprint import pprint
 
-with open('data_example.json') as data_file:    
+with open('dev-original.json') as data_file:    
     data = json.load(data_file)
 
-#pprint(data)
-#print (data)
-print("\n----------------------------------------C O N T E X T ---------------------------------------\n")
-print (data["data"][0]["paragraphs"][0]["context"])
-print("\n---------------------------------------- Q U E S T I O N S ---------------------------------------\n")
-print("el tamano del arreglo es: ", len(data["data"][0]["paragraphs"][0]["qas"]))
-for x in range(0, len(data["data"][0]["paragraphs"][0]["qas"])):
-	print(data["data"][0]["paragraphs"][0]["qas"][x]["question"])
-#    print "We're on time %d" % (x)
-#print(data["data"][0]["paragraphs"][0]["qas"][1]["question"])
+print("Nro de Parrafos:", len(data["data"]))
+for x in range(0, len(data["data"])):
+	print("\n ----------------------------------------C O N T E X T ---------------------------------------\n")
+	print(data["data"][x]["paragraphs"][x]["context"])
+	print("\n ----------------------------------------E N D   C O N T E X T ---------------------------------------\n")
 
+
+	print("Nro de Preguntas", len(data["data"][x]["paragraphs"][x]["qas"]))
+	print("\n---------------------------------------- Q U E S T I O N S ---------------------------------------\n")
+	for x in range(0, len(data["data"][x]["paragraphs"][x]["qas"])):
+		print(data["data"][0]["paragraphs"][0]["qas"][x]["question"])
+	print("\n---------------------------------------- E N D   Q U E S T I O N S ---------------------------------------\n")
