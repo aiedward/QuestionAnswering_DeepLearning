d61ef50901c58df402b5980f7f39cb4727792f7c
Johanna Chan <8nahchan8@gmail.com>
2017-02-27 15:25:58 -0400

    Actualización (util.py)

--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 import json
-#from pprint import pprint
+import re
 
 with open('dev-original.json') as data_file:    
     data = json.load(data_file)
 
-print("Nro de COntextos:", len(data["data"]))
+print("Nro de Contextos:", len(data["data"]))
 for a in range(0, len(data["data"])):
+	contexto = open('contexto.txt','w')
+	preguntas = open('preguntas.txt','w') 
 	print("------------------------------------------------------------------")
 	print(" \n C O N T E X T -")
 	print("------------------------------------------------------------------")
@@ -15,12 +17,25 @@ for a in range(0, len(data["data"])):
 		print(" \n P A R R A F O S -")
 		print("------------------------------------------------------------------")
 		print(data["data"][a]["paragraphs"][b]["context"])
+		print("ELIMINANDO PALABRAS")
+		algo = data["data"][a]["paragraphs"][b]["context"]
+		algo = re.sub(r'\b\w{1,2}\b', '', algo)
+		print 'algo vale:', algo
+		contexto.write(algo.encode('utf-8')) 
+		contexto.write("\n")
+		contexto.write("\n")
 		print("------------------------------------------------------------------")
 		print(" Q U E S T I O N S -")
 		print("------------------------------------------------------------------")
 		print("Nro de Preguntas:", len(data["data"][a]["paragraphs"][b]["qas"]))
 		for c in range(0, len(data["data"][a]["paragraphs"][b]["qas"])):
 			print(data["data"][a]["paragraphs"][b]["qas"][c]["question"])
+			print("ELIMINANDO PALABRAS DE PREGUNTAS")
+			algo = data["data"][a]["paragraphs"][b]["qas"][c]["question"]
+			algo = re.sub(r'\b\w{1,2}\b', '', algo)
+			print 'PREGUNTA algo vale:', algo
+			preguntas.write(algo.encode('utf-8'))
+			preguntas.write("\n")
 			print("------------------------------------------------------------------")
 		print(" E N D   Q U E S T I O N S  -")
 		print("------------------------------------------------------------------")
@@ -30,3 +45,5 @@ for a in range(0, len(data["data"])):
 	print("------------------------------------------------------------------")
 	print("E N D   C O N T E X T -")
 	print("------------------------------------------------------------------", a)
+contexto.close()
+preguntas.close()
