90abc0c486f428c4c06a3dde0cda52595146f7c3
Johanna Chan <8nahchan8@gmail.com>
2017-03-12 11:29:37 -0400

    Actualizacion Utils.py (12-03-2017 11:29)
    Generacion de CSV

--- util.py
+++ util.py
@@ -1,17 +1,33 @@
 import json
 import re
+import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#archivo csv
+context_data = open('context-data.csv', 'w')
+csvwriter = csv.writer(context_data, delimiter=" ")
+preguntas_data = open('preguntas.csv', 'w')
+csvwriterpreguntas = csv.writer(preguntas_data, delimiter=" ")
+
+count = 0
 
 with open('dev-original.json') as data_file:    
-    data = json.load(data_file)
-
+	data = json.load(data_file)
 print("Nro de Contextos:", len(data["data"]))
+
+#VOY A GENERAR EL ARCHIVO DE 50 TEXTOS
 for a in range(0, len(data["data"])):
 	contexto = open('contexto.txt','w')
-	preguntas = open('preguntas.txt','w') 
+	preguntas = open('preguntas.txt','w')
+	respuestas = open('respuestas.txt','w')
 	print("------------------------------------------------------------------")
 	print(" \n C O N T E X T -")
 	print("------------------------------------------------------------------")
+	#CONFIGURACION PARA 50 parrafos
 	print("Nro de Parrafos:", len(data["data"][a]["paragraphs"]))
+	print("Nro de COntextos:", len(data["data"]))
 	for b in range(0, len(data["data"][a]["paragraphs"])):
 		print("------------------------------------------------------------------")
 		print(" \n P A R R A F O S -")
@@ -19,8 +35,9 @@ for a in range(0, len(data["data"])):
 		print(data["data"][a]["paragraphs"][b]["context"])
 		print("ELIMINANDO PALABRAS")
 		algo = data["data"][a]["paragraphs"][b]["context"]
-		algo = re.sub(r'\b\w{1,2}\b', '', algo)
-		print 'algo vale:', algo
+		csvwriter.writerow([algo])
+		#algo = re.sub(r'\b\w{1,2}\b', '', algo)
+		#print 'algo vale:', algo
 		contexto.write(algo.encode('utf-8')) 
 		contexto.write("\n")
 		contexto.write("\n")
@@ -29,16 +46,27 @@ for a in range(0, len(data["data"])):
 		print("------------------------------------------------------------------")
 		print("Nro de Preguntas:", len(data["data"][a]["paragraphs"][b]["qas"]))
 		for c in range(0, len(data["data"][a]["paragraphs"][b]["qas"])):
+			print("------------------------------------------------------------------")
+			print("R E S P U E S T A S")
+			print("------------------------------------------------------------------")
+			for d in range(0, len(data["data"][a]["paragraphs"][b]["qas"][c]["answers"])):
+				print(data["data"][a]["paragraphs"][b]["qas"][c]["answers"][d]["answer_start"])
+				print(data["data"][a]["paragraphs"][b]["qas"][c]["answers"][d]["text"])
+			print("------------------------------------------------------------------")
+			print("R E S P U E S T A S")
+			print("------------------------------------------------------------------")
 			print(data["data"][a]["paragraphs"][b]["qas"][c]["question"])
 			print("ELIMINANDO PALABRAS DE PREGUNTAS")
 			algo = data["data"][a]["paragraphs"][b]["qas"][c]["question"]
-			algo = re.sub(r'\b\w{1,2}\b', '', algo)
+			csvwriterpreguntas.writerow([algo])
+			#algo = re.sub(r'\b\w{1,2}\b', '', algo)
 			print 'PREGUNTA algo vale:', algo
 			preguntas.write(algo.encode('utf-8'))
 			preguntas.write("\n")
-			print("------------------------------------------------------------------")
+		print("------------------------------------------------------------------")
 		print(" E N D   Q U E S T I O N S  -")
 		print("------------------------------------------------------------------")
+		++count
 	print("------------------------------------------------------------------")
 	print(" \n P A R R A F O S -")
 	print("------------------------------------------------------------------")
@@ -47,3 +75,5 @@ for a in range(0, len(data["data"])):
 	print("------------------------------------------------------------------", a)
 contexto.close()
 preguntas.close()
+context_data.close()
+preguntas_data.close()
