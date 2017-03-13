5a34e856fec7203ce8c94b99f5ee8d297f3317b6
Johanna Chan <8nahchan8@gmail.com>
2017-03-13 12:19:04 -0400

    Actualizacion test-train

--- test-train.py
+++ test-train.py
@@ -14,13 +14,13 @@ from rnn_theano import RNNTheano
 
 _VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '1539'))
 _HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '80'))
-_LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.001'))
+_LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.005'))
 _NEPOCH = int(os.environ.get('NEPOCH', '100'))
 _MODEL_FILE = os.environ.get('MODEL_FILE')
 
 def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
     # We keep track of the losses so we can plot them later
-    losses = []
+    losses = [] 
     num_examples_seen = 0
     for epoch in range(nepoch):
         # Optionally evaluate the loss
@@ -41,6 +41,28 @@ def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evalu
             # One SGD step
             model.sgd_step(X_train[i], y_train[i], learning_rate)
             num_examples_seen += 1
+
+def generate_sentence(model,word):
+    
+    # find in index word
+    wordFind= [word_to_index[word]]
+    predict = model.forward_propagation(wordFind)
+    samples = np.random.multinomial(1, predict[-1])
+    sampled_word = np.argmax(samples)
+    print("Prediccion : ")
+    print(sampled_word)
+    #nextWord=index_to_word[sampled_word]
+    print (index_to_word[sampled_word])
+    res=[]
+    # We start the sentence with the start t
+    for sent in sentences:
+        esta= sent.find(nextWord)
+        if esta>0:
+            res.append(sent)
+
+    return res        
+
+
 
 vocabulary_size = _VOCABULARY_SIZE
 unknown_token = "UNKNOWN_TOKEN"
@@ -69,6 +91,8 @@ print "Found %d unique words tokens." % len(word_freq.items())
 vocab = word_freq.most_common(vocabulary_size-1)
 index_to_word = [x[0] for x in vocab]
 index_to_word.append(unknown_token)
+print "Tamano desl vocabulario"
+print (len(index_to_word))
 word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
 print "Using vocabulary size %d." % vocabulary_size
@@ -82,7 +106,7 @@ for i, sent in enumerate(tokenized_sentences):
 X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
 y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
 
-
+np.random.seed(10)
 model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
 t1 = time.time()
 model.sgd_step(X_train[10], y_train[10], _LEARNING_RATE)
@@ -92,4 +116,43 @@ print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
 if _MODEL_FILE != None:
     load_model_parameters_theano(_MODEL_FILE, model)
 
-train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
+#train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
+load_model_parameters_theano('./data/rnn-theano-80-1539-2017-03-13-11-22-47.npz', model)
+
+print("Length index_word")
+print "---------------------------------------------------------------"
+#word = raw_input("Introduzca la palabra clave de la pregunta a buscar  \n")  
+
+
+
+#res= generate_sentence(model,word)
+#print "Frases donde se ubica la respuesta"
+#print(res)
+
+
+def generate_sentence(model,word):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index["denver"]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+            print"Prediccion"
+            print(sampled_word)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+ 
+num_sentences = 10
+senten_min_length = 3
+ 
+for i in range(1):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model,"word")
+    print " ".join(sent)
