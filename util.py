be8a7d6c64386c9c4acb7a39752a0ec4e877d42c
Johanna Chan <8nahchan8@gmail.com>
2017-03-19 11:02:40 -0400

    Actualizacion de utils para generacion de nuevos pares
    csv con texto+preguntas y respuestas

--- util.py
+++ util.py
@@ -6,20 +6,23 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #archivo csv
-all_data = open('all.csv', 'w')
-csvwriterall = csv.writer(all_data, delimiter=" ")
+#all_data = open('all.csv', 'w')
+#csvwriterall = csv.writer(all_data, delimiter=" ")
 
-context_data = open('context.csv', 'w')
-csvwriter = csv.writer(context_data, delimiter=" ")
+#context_data = open('context.csv', 'w')
+#csvwriter = csv.writer(context_data, delimiter=" ")
 
-preguntas_data = open('preguntas.csv', 'w')
-csvwriterpreguntas = csv.writer(preguntas_data, delimiter=" ")
+#preguntas_data = open('preguntas.csv', 'w')
+#csvwriterpreguntas = csv.writer(preguntas_data, delimiter=" ")
 
-respuestas_data = open('respuestas.csv', 'w')
-csvwriterrespuestas = csv.writer(respuestas_data, delimiter=" ")
+#respuestas_data = open('respuestas.csv', 'w')
+#csvwriterrespuestas = csv.writer(respuestas_data, delimiter=" ")
 
 preguntasyrespuestas_data = open('preguntasyrespuestas.csv', 'w')
 csvwriterpreguntasyrespuestas = csv.writer(preguntasyrespuestas_data, delimiter=" ")
+
+textomaspreguntasyrespuestas_data = open('textomaspreguntasyrespuestas.csv', 'w')
+csvwritertextomaspreguntasyrespuestas = csv.writer(textomaspreguntasyrespuestas_data, delimiter=" ")
 
 count = 0
 
@@ -35,6 +38,7 @@ for a in range(0, 1):
 	#CONFIGURACION PARA 50 parrafos
 	print("Nro de Parrafos:", len(data["data"][a]["paragraphs"]))
 	print("Nro de COntextos:", len(data["data"]))
+	textomaspreguntas = ""
 	for b in range(0, 50):
 		print("------------------------------------------------------------------")
 		print(" \n P A R R A F O S -")
@@ -42,8 +46,16 @@ for a in range(0, 1):
 		print(data["data"][a]["paragraphs"][b]["context"])
 		print("ELIMINANDO PALABRAS")
 		algo = data["data"][a]["paragraphs"][b]["context"]
-		csvwriterall.writerow([algo])
-		csvwriter.writerow([algo])
+		textomaspreguntas += data["data"][a]["paragraphs"][b]["context"]
+		textomaspreguntas += " "
+#		csvwriterall.writerow([algo])
+#		csvwriter.writerow([algo])
+
+#ESTO LO VOY A ESCRIBIR MAS ABAJO CUANDO LEA TODAS LAS PREGUNTAS
+#		csvwritertextomaspreguntasyrespuestas.writerow([algo])
+
+
+
 		#algo = re.sub(r'\b\w{1,2}\b', '', algo)
 		#print 'algo vale:', algo
 #		contexto.write(algo.encode('utf-8')) 
@@ -58,9 +70,17 @@ for a in range(0, 1):
 			print(data["data"][a]["paragraphs"][b]["qas"][c]["question"])
 			print("ELIMINANDO PALABRAS DE PREGUNTAS")
 			algo = data["data"][a]["paragraphs"][b]["qas"][c]["question"]
-			csvwriterall.writerow([algo])
-			csvwriterpreguntas.writerow([algo])			
+			textomaspreguntas += data["data"][a]["paragraphs"][b]["qas"][c]["question"]
+			textomaspreguntas += " "
+#			csvwriterall.writerow([algo])
+#			csvwriterpreguntas.writerow([algo])			
 			csvwriterpreguntasyrespuestas.writerow([algo])
+
+#no me conviene guardar aqui 
+
+#			csvwritertextomaspreguntasyrespuestas.writerow([algo])
+
+
 			#algo = re.sub(r'\b\w{1,2}\b', '', algo)
 			print 'PREGUNTA algo vale:', algo
 #			preguntas.write(algo.encode('utf-8'))
@@ -71,6 +91,7 @@ for a in range(0, 1):
 			algo = set()
 			count = 0
 			ora_comp = ""
+			csvwritertextomaspreguntasyrespuestas.writerow([textomaspreguntas])
 			for d in range(0, len(data["data"][a]["paragraphs"][b]["qas"][c]["answers"])):
 				print(data["data"][a]["paragraphs"][b]["qas"][c]["answers"][d]["answer_start"])
 				print(data["data"][a]["paragraphs"][b]["qas"][c]["answers"][d]["text"])
@@ -85,9 +106,10 @@ for a in range(0, 1):
 						++count
 			ora_comp += ', '.join(algo)
 			print (ora_comp)
-			csvwriterall.writerow([ora_comp])
-			csvwriterrespuestas.writerow([ora_comp])
+#			csvwriterall.writerow([ora_comp])
+#			csvwriterrespuestas.writerow([ora_comp])
 			csvwriterpreguntasyrespuestas.writerow([ora_comp])
+			csvwritertextomaspreguntasyrespuestas.writerow([ora_comp])
 			print("------------------------------------------------------------------")
 			print("R E S P U E S T A S")
 			print("------------------------------------------------------------------")
@@ -101,8 +123,9 @@ for a in range(0, 1):
 	print("------------------------------------------------------------------")
 	print("E N D   C O N T E X T -")
 	print("------------------------------------------------------------------", a)
-all_data.close()
-context_data.close()
-preguntas_data.close()
-respuestas_data.close()
+#all_data.close()
+#context_data.close()
+#preguntas_data.close()
+#respuestas_data.close()
 preguntasyrespuestas_data.close()
+textomaspreguntasyrespuestas_data.close()
