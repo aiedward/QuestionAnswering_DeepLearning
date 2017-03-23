03ef8549a33b2ddce3446ab365d8108827259dd9
Nilver Viera <nilverv@gmail.com>
2017-03-23 06:05:55 -0400

    Uso de preguntasyrespuestascsv

--- test-train.py
+++ test-train.py
@@ -6,15 +6,13 @@ import operator
 import numpy as np
 import nltk
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import os
 import time
 from datetime import datetime
 from utils import *
 from rnn_theano import RNNTheano
 
-_VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '1539'))
+_VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '18000'))
 _HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '80'))
 _LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.005'))
 _NEPOCH = int(os.environ.get('NEPOCH', '100'))
@@ -22,7 +20,7 @@ _MODEL_FILE = os.environ.get('MODEL_FILE')
 
 def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
     # We keep track of the losses so we can plot them later
-    losses = [] 
+    losses = []
     num_examples_seen = 0
     for epoch in range(nepoch):
         # Optionally evaluate the loss
@@ -44,28 +42,6 @@ def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evalu
             model.sgd_step(X_train[i], y_train[i], learning_rate)
             num_examples_seen += 1
 
-def generate_sentence(model,word):
-    
-    # find in index word
-    wordFind= [word_to_index[word]]
-    predict = model.forward_propagation(wordFind)
-    samples = np.random.multinomial(1, predict[-1])
-    sampled_word = np.argmax(samples)
-    print("Prediccion : ")
-    print(sampled_word)
-    #nextWord=index_to_word[sampled_word]
-    print (index_to_word[sampled_word])
-    res=[]
-    # We start the sentence with the start t
-    for sent in sentences:
-        esta= sent.find(nextWord)
-        if esta>0:
-            res.append(sent)
-
-    return res        
-
-
-
 vocabulary_size = _VOCABULARY_SIZE
 unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token = "SENTENCE_START"
@@ -85,115 +61,84 @@ print "Parsed %d sentences." % (len(sentences))
 # Tokenize the sentences into words
 tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
 
-# Count the word frequencies
-word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
 
-# Get the most common words and build index_to_word and word_to_index vectors
-vocab = word_freq.most_common(vocabulary_size-1)
-index_to_word = [x[0] for x in vocab]
-index_to_word.append(unknown_token)
-print "Tamano desl vocabulario"
-print (len(index_to_word))
+index_to_word1=[]
+#index_to_word1.append(unknown_token)
+
+print"--------------------index_to_word "
+
+index_to_word=[unknown_token]
+
+for sent in tokenized_sentences:
+    index_to_word.extend(sent)
+
+
 word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+print(word_to_index[unknown_token])   
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
-
-# Replace all words not in our vocabulary with the unknown token
-for i, sent in enumerate(tokenized_sentences):
-    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
-
-# Create the training data
-# Create the training data
 arreglo = []
 x = []
 y = []
-print("---------------------------------------------------------------------------")
-par = 0;
+#print("---------------------------------------------------------------------------")
+par = 0
+anterior = 0
 for sent in tokenized_sentences:
     arreglo = []
-    for w in sent[:-1]:
+    for w in sent:
         if w in word_to_index:
             arreglo.append(word_to_index[w])
-    print arreglo
+#    print arreglo
+
+        if len(sent)>=60:
+            print("A LE RTA AAAAAAAAAAAAAAAAAAAAAAAAAaa")
+    while (len(arreglo)< 60) :
+        a=word_to_index[unknown_token]
+        arreglo.append(a)
+
+
     if par % 2 == 0:
         x.append(arreglo)
     else:
         y.append(arreglo)
     par = par + 1
 
+#print("---------------------------------------------------------------------------")
+
+x1 = np.asarray([sent for sent in x])
+X_train =  x1.astype(np.int32)
+y1 = np.asarray([sent for sent in y])
+y_train =  y1.astype(np.int32)
+#Create the training data
+#X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+#y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
 print("---------------------------------------------------------------------------")
+print("E N T R A D A len")
+print(type(X_train))
+#print("---------------------------------------------------------------------------")
+#for a in X_train:
+print (len(X_train))
+print (X_train)
+#print("---------------------------------------------------------------------------")
+print("S A L I D A")
+#print("---------------------------------------------------------------------------")
+#for a in y_train:
+print (len(y_train))
+print (y_train)
 
-<<<<<<< HEAD
-=======
-X_train = np.asarray(x)
-y_train = np.asarray(y)
 
->>>>>>> be8a7d6c64386c9c4acb7a39752a0ec4e877d42c
-np.random.seed(10)
+ 
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+
 model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
-t1 = time.time()
-model.sgd_step(X_train[10], y_train[10], _LEARNING_RATE)
-t2 = time.time()
-print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
 
 if _MODEL_FILE != None:
     load_model_parameters_theano(_MODEL_FILE, model)
 
-#train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
-load_model_parameters_theano('./data/rnn-theano-80-1539-2017-03-13-11-22-47.npz', model)
-
-print("Length index_word")
-print "---------------------------------------------------------------"
-#word = raw_input("Introduzca la palabra clave de la pregunta a buscar  \n")  
-
-
-
-#res= generate_sentence(model,word)
-#print "Frases donde se ubica la respuesta"
-#print(res)
-
-
-def generate_sentence(model,word):
-    # We start the sentence with the start token
-    new_sentence = [word_to_index["denver"]]
-    # Repeat until we get an end token
-    while not new_sentence[-1] == word_to_index[sentence_end_token]:
-        next_word_probs = model.forward_propagation(new_sentence)
-        sampled_word = word_to_index[unknown_token]
-        # We don't want to sample unknown words
-        while sampled_word == word_to_index[unknown_token]:
-            samples = np.random.multinomial(1, next_word_probs[-1])
-            sampled_word = np.argmax(samples)
-            print"Prediccion"
-            print(sampled_word)
-        new_sentence.append(sampled_word)
-    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
-    return sentence_str
- 
-num_sentences = 10
-senten_min_length = 3
- 
-for i in range(1):
-    sent = []
-    # We want long sentences, not sentences with one or two words
-    while len(sent) < senten_min_length:
-        sent = generate_sentence(model,"word")
-<<<<<<< HEAD
-    print " ".join(sent)
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-    print " ".join(sent)
->>>>>>> be8a7d6c64386c9c4acb7a39752a0ec4e877d42c
+train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
